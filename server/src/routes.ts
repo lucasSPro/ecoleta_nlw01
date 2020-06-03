83dcffc072ee634b85d912a4286756dc29aabe23
lucasSPro <lucas.san.77@gmail.com>
2020-06-03 07:33:15 -0300

    add structure, database & first route on server

---
+++ server/src/routes.ts
@@ -0,0 +1,19 @@
+import express, { response } from 'express';
+import Knex from './database/connection';
+
+const routes = express.Router();
+
+
+routes.get('/items', async (request, response) => {
+  const items = await Knex('items').select('*');
+
+  const serializedItems = items.map( item => {
+    return {
+      title: item.title,
+      image_url: `http://localhost:3334/uploads/${item.image}`,
+    };
+  });
+  return response.json(serializedItems);
+ });
+
+ export default routes;
